2174457ba4f65b1c3bf28ee58989d4317da48843
yangxinxiao <yangxinxiao@123juzi.com>
2016-10-17 16:25:57 +0800

    [master]增加获取最大优先级的方法。

---
+++ app/src/main/java/com/vc/wifiscanner/common/CommonData.java
@@ -0,0 +1,10 @@
+package com.vc.wifiscanner.common;
+
+/**
+ * Created by sw_01 on 2016/10/17.
+ */
+
+public class CommonData {
+
+    public static String WIFI_NAME = "DRAGONSCANNER";
+}
--- app/src/main/java/com/vc/wifiscanner/utils/WifiScanUtils.java
+++ app/src/main/java/com/vc/wifiscanner/utils/WifiScanUtils.java
@@ -1,9 +1,12 @@
 package com.vc.wifiscanner.utils;
 
+import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiManager;
 
 import com.vc.wifiscanner.WifiScannerActivity;
 import com.vc.wifiscanner.manager.ConfigManager;
+
+import java.util.List;
 
 import static android.content.Context.WIFI_SERVICE;
 
@@ -17,5 +20,19 @@ public class WifiScanUtils {
             wifiManager.setWifiEnabled(true);
         }
         wifiManager.startScan();
+    }
+
+    public static int getConfigWifiMaxPriority(WifiManager wifiManager){
+        int maxPri = 0;
+        List<WifiConfiguration> configurations =  wifiManager.getConfiguredNetworks();
+        if (null == configurations){
+            return maxPri;
+        }
+        for (WifiConfiguration conf:configurations) {
+            if (conf.priority >= maxPri ){
+                maxPri = conf.priority;
+            }
+        }
+        return maxPri;
     }
 }
